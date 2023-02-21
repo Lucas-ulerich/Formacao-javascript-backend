572acca0de13d0c0c8a7251866b8f7c87dc0d9d5
Lucas-ulerich <lucas.ulerichsouza@gmail.com>
2023-02-21 11:04:49 -0300

    Iniciando curso de javascript para back-end

---
+++ Tipos primitivos/type-number.js
@@ -0,0 +1,25 @@
+// Tipo Number
+// O tipo Number armazena valores numerios, inteiros, reais, positivos e negativos.
+
+
+const meuNumero = 5;
+
+const primeiroNumero = 2;
+const segundoNumero = 3;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// Ponto flutuante (Numeros reais)
+
+const meuPontoFlutuante = 3.3
+const pontoFlutuanteSemZero = .5
+
+const operacaoFlutuante = primeiroNumero / meuPontoFlutuante
+console.log(operacaoFlutuante)
+
+// NaN -> Not A Number (não é um numero)
+
+const texto = 'Alura'
+console.log(primeiroNumero * texto)
---
+++ Tipos primitivos/type-string.js
@@ -0,0 +1,20 @@
+// String guarda dados do tipos texto e caracteres. 
+
+//Podemos usar aspas duplas ("") ou aspas simples ('') para demarcar um texto
+
+const texto1 = 'Olá mundo!';
+const texto2 = "olá mundo!";
+
+//Numeros entre aspas é considerados string
+
+const stringDeNumeros = '34567';
+
+
+
+// Concatenacao (+)
+const nome = 'Lucas';
+const citacao = 'Meu nome é: ';
+console.log(citacao);
+console.log(citacao + nome);
+
+//Template string ou template literal
